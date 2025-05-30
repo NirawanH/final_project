58deaabd3f746630ead7108ac3693df1d6693d11
NirawanH <nirawan.ht@gmail.com>
2025-05-30 21:33:06 +0200

    test_to_from_dict() in test_TaskList.py

--- tests/test_TaskList.py
+++ tests/test_TaskList.py
@@ -1,6 +1,7 @@
 import pytest
 from src.TaskList import TaskList
 from src.Card import Card
+from datetime import datetime
 
     
 @pytest.fixture(autouse=True)
@@ -46,6 +47,31 @@ def test_get_card_not_found():
     task_list = TaskList("To-do")
     result = task_list.get_card(3)
     assert result is None
+
+def test_to_from_dict():
+    task_list = TaskList("To-do")
+    card1 = Card("Project", "P")
+    card1.set_deadline("22/06/2025 20:25")
+    task_list.add_card(card1)
+    card2 = Card("Two", "T")
+    card2.set_deadline("24/07/2027 17:30")
+    task_list.add_card(card2)
+
+    data = task_list.to_dict()
+    load = TaskList.from_dict(data)
+
+    assert load.list_name == "To-do"
+    assert len(load.cards) == 2
+    assert load.cards[0].title == "Project"
+    assert load.cards[0].description == "P"
+    assert load.cards[0].deadline == datetime(2025,6,22,20,25)
+    assert load.cards[1].title == "Two"
+    assert load.cards[1].description == "T"
+    assert load.cards[1].deadline == datetime(2027,7,24,17,30)
+
+
+    
+    
 
     
 
